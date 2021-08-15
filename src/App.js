305ca948ad6e162735b17ca728b2e8f2a40bf474
Aditya Chaphekar <adityachaps@gmail.com>
2021-08-15 15:28:43 +0530

    Initial Setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <title>Aditya Chaphekar</title>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
